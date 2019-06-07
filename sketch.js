32332a1de86acb6be56a595c045ac0e6aaf7b0ae
ethafa <31740787+ethafa@users.noreply.github.com>
2019-06-07 17:35:18 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -66,14 +66,14 @@ function draw() {
   displayScreen();
 
   if (gameScreen == 15) {
-    if (clickStatus) {
+    if (receiveMessage == Tag13) {
       gameScreen = 17;
       console.log("vado a screen " + gameScreen);
       clickStatus = false;
     }
   }
   if (gameScreen == 17) {
-    if (clickStatus) {
+    if (receiveMessage == Tag13) {
       gameScreen = 0;
       console.log("vado a screen " + gameScreen);
       clickStatus = false;
